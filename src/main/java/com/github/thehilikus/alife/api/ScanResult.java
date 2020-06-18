65cadcb4512a7819428193b37caec6cf6d35fdec
Alejandro Endo <theHilikus@gmail.com>
2020-06-18 01:22:46 -0400

    Make scan result keep the x and y distance separately
    This way it is easier to decide which direction to go

--- src/main/java/com/github/thehilikus/alife/api/ScanResult.java
+++ src/main/java/com/github/thehilikus/alife/api/ScanResult.java
@@ -4,21 +4,48 @@ package com.github.thehilikus.alife.api;
  * Each agent found in a scan
  */
 public class ScanResult implements Comparable<ScanResult> {
-    private final int distanceSquared;
+    private final int xDistance;
+    private final int yDistance;
     private final Agent agent;
+    private final boolean isEdge;
 
-    public ScanResult(int distanceSquared, Agent agent) {
-        this.distanceSquared = distanceSquared;
+    public ScanResult(int xDistance, int yDistance) {
+        this(xDistance, yDistance, null, true);
+    }
+
+    public ScanResult(int xDistance, int yDistance, Agent agent) {
+        this(xDistance, yDistance, agent, false);
+    }
+
+    private ScanResult(int xDistance, int yDistance, Agent agent, boolean isEdge) {
+        if (xDistance < 0 || yDistance < 0) {
+            throw new IllegalArgumentException("Distance has to be an absolute value");
+        }
+        this.xDistance = xDistance;
+        this.yDistance = yDistance;
         this.agent = agent;
+        this.isEdge = isEdge;
     }
 
     @Override
     public int compareTo(ScanResult other) {
-        return distanceSquared - other.distanceSquared;
+        int distanceSquared = (xDistance << 1) + (yDistance << 1);
+        int otherDistanceSquared = (other.xDistance << 1) + (other.yDistance << 1);
+
+        return distanceSquared - otherDistanceSquared;
     }
 
-    public double getDistanceSquared() {
-        return distanceSquared;
+
+    public int getXDistance() {
+        return xDistance;
+    }
+
+    public int getYDistance() {
+        return yDistance;
+    }
+
+    public boolean isEdge() {
+        return isEdge;
     }
 
     public Agent getAgent() {
