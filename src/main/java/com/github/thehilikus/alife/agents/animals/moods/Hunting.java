09c0ddc2d7fd38c37d7ed6ba1dc5814ced33ffa6
Alejandro Endo <theHilikus@gmail.com>
2020-06-18 00:32:10 -0400

    First implementation of hunting

--- src/main/java/com/github/thehilikus/alife/agents/animals/moods/Hunting.java
+++ src/main/java/com/github/thehilikus/alife/agents/animals/moods/Hunting.java
@@ -2,44 +2,110 @@ package com.github.thehilikus.alife.agents.animals.moods;
 
 import com.diogonunes.jcdp.color.api.Ansi;
 import com.github.thehilikus.alife.agents.controllers.MoodController;
-import com.github.thehilikus.alife.api.Agent;
-import com.github.thehilikus.alife.api.Mood;
-import com.github.thehilikus.alife.api.Locomotion;
-import com.github.thehilikus.alife.api.Vision;
+import com.github.thehilikus.alife.agents.genetics.Genome;
+import com.github.thehilikus.alife.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
+import java.util.Optional;
+import java.util.SortedSet;
 
 /**
  * When food has been located
  */
 public class Hunting implements Mood {
-    public Hunting(MoodController moodController, Vision vision, Locomotion locomotion, Agent foodAgent) {
+    private static final Logger LOG = LoggerFactory.getLogger(Hunting.class);
+    private final MoodController moodController;
+    private final int agentId;
+    private final Vision vision;
+    private final Locomotion locomotion;
+    private final double speedFactor;
+    private final Existing existing;
+    private final Agent target;
+    private int lastMovement;
 
+    public Hunting(MoodController moodController, Vision vision, Locomotion locomotion, Genome genome, Agent foodAgent) {
+        this.moodController = moodController;
+        this.agentId = vision.getAgentId();
+        this.vision = vision;
+        this.locomotion = locomotion;
+        this.speedFactor = genome.getGene(Locomotion.PARAMETER_PREFIX + "huntSpeedFactor");
+        this.existing = new Existing(agentId);
+        this.target = foodAgent;
     }
 
     @Override
     public int getAgentId() {
-        throw new UnsupportedOperationException("Not implemented yet"); //TODO: implement
+        return agentId;
     }
 
     @Override
     public Map<String, String> getParameters() {
-        throw new UnsupportedOperationException("Not implemented yet"); //TODO: implement
+        return Map.of(
+                PARAMETER_PREFIX + "current", getClass().getSimpleName(),
+                PARAMETER_PREFIX + "huntSpeedFactor", Double.toString(speedFactor),
+                PARAMETER_PREFIX + "lastMovement", Integer.toString(lastMovement)
+        );
     }
 
     @Override
     public Mood tick() {
-        throw new UnsupportedOperationException("Not implemented yet"); //TODO: implement
+        //scan to see if target is still there
+        SortedSet<ScanResult> scanResult = vision.scan(target.getClass());
+        Optional<ScanResult> targetOptional = scanResult.stream().filter(scan -> scan.getAgent().getId() == this.target.getId()).findFirst();
+        if (targetOptional.isPresent()) {
+            Orientation targetDirection = findTargetDirection();
+            lastMovement = locomotion.move(speedFactor, targetDirection);
+            //TODO: if we got there?
+        } else {
+            LOG.info("Target {} is gone :(", target.getId());
+            return moodController.startScouting();
+        }
+
+        return this;
+    }
+
+    private Orientation findTargetDirection() {
+        Position destination = target.getPosition();
+        Position.Immutable position = locomotion.getPosition();
+        int deltaX = destination.getX() - position.getX();
+        int deltaY = destination.getY() - position.getY();
+        Orientation result;
+        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+            //move in X
+            if (deltaX < 0) {
+                result = Orientation.WEST;
+            } else {
+                result = Orientation.EAST;
+            }
+        } else {
+            //move in Y
+            if (deltaY < 0) {
+                result = Orientation.NORTH;
+            } else {
+                result = Orientation.SOUTH;
+            }
+        }
+
+        return result;
     }
 
     @Override
     public int getHungerDelta() {
-        throw new UnsupportedOperationException("Not implemented yet"); //TODO: implement
+        return existing.getHungerDelta();
     }
 
     @Override
     public int getEnergyDelta() {
-        throw new UnsupportedOperationException("Not implemented yet"); //TODO: implement
+        return existing.getEnergyDelta() + (int) Math.round(lastMovement * locomotion.getEnergyExpenditureFactor());
+    }
+
+    @Override
+    public String toString() {
+        return "Hunting{" +
+                "target=" + target +
+                '}';
     }
 
     @Override
