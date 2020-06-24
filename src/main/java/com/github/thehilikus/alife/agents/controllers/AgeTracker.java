89a94b57a792e4d9e44cc01ce6b00887417ff38b
Alejandro Endo <theHilikus@gmail.com>
2020-06-24 01:11:31 -0400

    Make vitals Components
    That way they can integrate to the generic reporting mechanism

--- src/main/java/com/github/thehilikus/alife/agents/controllers/AgeTracker.java
+++ src/main/java/com/github/thehilikus/alife/agents/controllers/AgeTracker.java
@@ -1,16 +1,21 @@
 package com.github.thehilikus.alife.agents.controllers;
 
+import com.github.thehilikus.alife.api.Component;
 import com.github.thehilikus.alife.api.Mood;
 import com.github.thehilikus.alife.api.VitalSign;
+
+import java.util.Map;
 
 /**
  * Monitors the age of an agent
  */
-public class AgeTracker implements VitalSign {
+public class AgeTracker implements VitalSign, Component {
+    private final int agentId;
     private final int lifeExpectancy;
     private int currentAge;
 
-    public AgeTracker(int lifeExpectancy) {
+    public AgeTracker(int agentId, int lifeExpectancy) {
+        this.agentId = agentId;
         this.lifeExpectancy = lifeExpectancy;
     }
 
@@ -22,5 +27,15 @@ public class AgeTracker implements VitalSign {
     @Override
     public boolean isAlive() {
         return currentAge <= lifeExpectancy;
+    }
+
+    @Override
+    public int getAgentId() {
+        return agentId;
+    }
+
+    @Override
+    public Map<String, String> getParameters() {
+        return Map.of(PARAMETER_PREFIX + "age", Integer.toString(currentAge));
     }
 }
--- src/main/java/com/github/thehilikus/alife/agents/controllers/VitalsController.java
+++ src/main/java/com/github/thehilikus/alife/agents/controllers/VitalsController.java
@@ -1,21 +1,32 @@
 package com.github.thehilikus.alife.agents.controllers;
 
 import com.github.thehilikus.alife.agents.genetics.Genome;
+import com.github.thehilikus.alife.api.Component;
 import com.github.thehilikus.alife.api.Mood;
 import com.github.thehilikus.alife.api.VitalSign;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Aggregates the information of all the agent vitals
  */
-public class VitalsController {
+public class VitalsController implements Component {
+    private static final Logger LOG = LoggerFactory.getLogger(VitalsController.class);
     private final HungerTracker hungerTracker;
     private final EnergyTracker energyTracker;
     private final AgeTracker ageTracker;
+    private final int agentId;
+    private final MoodController moodController;
 
-    public VitalsController(Genome genome) {
-        this.hungerTracker = new HungerTracker();
-        this.energyTracker = new EnergyTracker();
-        this.ageTracker = new AgeTracker(genome.getGene(VitalSign.PARAMETER_PREFIX + "lifeExpectancy"));
+    public VitalsController(int agentId, MoodController moodController, Genome genome) {
+        this.agentId = agentId;
+        this.hungerTracker = new HungerTracker(agentId, genome);
+        this.moodController = moodController;
+        this.energyTracker = new EnergyTracker(agentId, genome);
+        this.ageTracker = new AgeTracker(agentId, genome.getGene(VitalSign.PARAMETER_PREFIX + "lifeExpectancy"));
     }
 
     public Mood update(Mood lastMood) {
@@ -29,5 +40,20 @@ public class VitalsController {
 
     public boolean isAlive() {
         return hungerTracker.isAlive() || energyTracker.isAlive() || ageTracker.isAlive();
+    }
+
+    @Override
+    public int getAgentId() {
+        return agentId;
+    }
+
+    @Override
+    public Map<String, String> getParameters() {
+        Map<String, String> result = new LinkedHashMap<>();
+        result.putAll(hungerTracker.getParameters());
+        result.putAll(energyTracker.getParameters());
+        result.putAll(ageTracker.getParameters());
+
+        return result;
     }
 }
