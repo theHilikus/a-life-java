4c03a8e8101597ad4aa9773ccc621c348c66bcce
Alejandro Endo <alejandro@hivebuzzer.com>
2020-06-24 01:11:31 -0400

    Make vitals Components
    That way they can integrate to the generic reporting mechanism

--- src/main/java/com/github/thehilikus/alife/agents/controllers/EnergyTracker.java
+++ src/main/java/com/github/thehilikus/alife/agents/controllers/EnergyTracker.java
@@ -1,15 +1,21 @@
 package com.github.thehilikus.alife.agents.controllers;
 
+import com.github.thehilikus.alife.agents.genetics.Genome;
+import com.github.thehilikus.alife.api.Component;
 import com.github.thehilikus.alife.api.Mood;
 import com.github.thehilikus.alife.api.VitalSign;
+
+import java.util.Map;
 
 /**
  * Monitors the agent's level of energy
  */
-public class EnergyTracker implements VitalSign {
+public class EnergyTracker implements VitalSign, Component {
+    private final int agentId;
     private int currentEnergy;
 
-    public EnergyTracker() {
+    public EnergyTracker(int agentId, Genome genome) {
+        this.agentId = agentId;
         currentEnergy = 100;
     }
 
@@ -25,5 +31,15 @@ public class EnergyTracker implements VitalSign {
     @Override
     public boolean isAlive() {
         return currentEnergy > 0;
+    }
+
+    @Override
+    public int getAgentId() {
+        return agentId;
+    }
+
+    @Override
+    public Map<String, String> getParameters() {
+        return Map.of(PARAMETER_PREFIX + "energy", Integer.toString(currentEnergy));
     }
 }
--- src/main/java/com/github/thehilikus/alife/agents/controllers/HungerTracker.java
+++ src/main/java/com/github/thehilikus/alife/agents/controllers/HungerTracker.java
@@ -1,16 +1,23 @@
 package com.github.thehilikus.alife.agents.controllers;
 
+import com.github.thehilikus.alife.agents.genetics.Genome;
+import com.github.thehilikus.alife.api.Component;
 import com.github.thehilikus.alife.api.Mood;
 import com.github.thehilikus.alife.api.VitalSign;
+
+import java.util.Map;
 
 /**
  * Monitors the hunger of an agent
  */
-public class HungerTracker implements VitalSign {
+public class HungerTracker implements VitalSign, Component {
     private static final int STARTING_HUNGER = 50;
+    private final int agentId;
     private int currentHunger;
+    private final int hungryThreshold;
 
-    public HungerTracker() {
+    public HungerTracker(int agentId, Genome genome) {
+        this.agentId = agentId;
         currentHunger = STARTING_HUNGER;
     }
 
@@ -22,5 +29,15 @@ public class HungerTracker implements VitalSign {
     @Override
     public boolean isAlive() {
         return currentHunger > 0;
+    }
+
+    @Override
+    public int getAgentId() {
+        return agentId;
+    }
+
+    @Override
+    public Map<String, String> getParameters() {
+        return Map.of(PARAMETER_PREFIX + "hunger", Integer.toString(currentHunger));
     }
 }
