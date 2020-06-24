ad852e767adfecf84c9b35c6475550d28f85f5ad
Alejandro Endo <theHilikus@gmail.com>
2020-06-23 21:09:20 -0400

    Implement vitals trackers
    These trackes measure values that affect an agent independent of mood

---
+++ src/main/java/com/github/thehilikus/alife/agents/controllers/VitalsController.java
@@ -0,0 +1,33 @@
+package com.github.thehilikus.alife.agents.controllers;
+
+import com.github.thehilikus.alife.agents.genetics.Genome;
+import com.github.thehilikus.alife.api.Mood;
+import com.github.thehilikus.alife.api.VitalSign;
+
+/**
+ * Aggregates the information of all the agent vitals
+ */
+public class VitalsController {
+    private final HungerTracker hungerTracker;
+    private final EnergyTracker energyTracker;
+    private final AgeTracker ageTracker;
+
+    public VitalsController(Genome genome) {
+        this.hungerTracker = new HungerTracker();
+        this.energyTracker = new EnergyTracker();
+        this.ageTracker = new AgeTracker(genome.getGene(VitalSign.PARAMETER_PREFIX + "lifeExpectancy"));
+    }
+
+    public Mood update(Mood lastMood) {
+        hungerTracker.update(lastMood);
+        energyTracker.update(lastMood);
+        ageTracker.update();
+        //TODO: find proper mood
+
+        return lastMood;
+    }
+
+    public boolean isDead() {
+        return hungerTracker.isDead() || energyTracker.isDead() || ageTracker.isDead();
+    }
+}
---
+++ src/main/java/com/github/thehilikus/alife/api/VitalSign.java
@@ -0,0 +1,19 @@
+package com.github.thehilikus.alife.api;
+
+/**
+ * A metric that affects the agent regardless of mood
+ */
+public interface VitalSign {
+    /**
+     * The prefix for vital sign parameters
+     */
+    String PARAMETER_PREFIX = "vital.";
+
+    void update(Mood currentMood);
+
+    default void update() {
+        update(null);
+    }
+
+    boolean isDead();
+}
