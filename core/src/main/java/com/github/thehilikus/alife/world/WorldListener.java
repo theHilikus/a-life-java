28893aa97e622af1b9e19fc1e51f484312dafee9
Alejandro Endo <theHilikus@gmail.com>
2023-06-25 21:32:18 -0400

    Add javadoc to WorldListener

--- core/src/main/java/com/github/thehilikus/alife/world/WorldListener.java
+++ core/src/main/java/com/github/thehilikus/alife/world/WorldListener.java
@@ -9,10 +9,24 @@ import java.util.Optional;
  * Callback to notify of world events
  */
 public interface WorldListener {
+    /**
+     * Called after every tick of the clock
+     *
+     * @param latestStatus the current status of the world after the tick completed
+     * @return true if the simulation should continue
+     */
     boolean ticked(WorldStatus latestStatus);
 
+    /**
+     * Called when the simulation has ended
+     *
+     * @param hour the age of the world when the simulation ended
+     */
     void ended(int hour);
 
+    /**
+     * Read-only interface to the status of the world
+     */
     interface WorldStatus {
         int getWidth();
 
